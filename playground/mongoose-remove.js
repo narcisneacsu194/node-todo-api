6433eba68c92e87e6a9b2cd897b3b168ea5f930d
narcisneacsu194 <narcisneacsu194@gmail.com>
2018-11-12 00:50:06 +0200

    Added DELETE /todos/:id endpoint.

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,13 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+Todo.findByIdAndRemove('5be8ab887121859e13af9de3').then((todo) => {
+  console.log(todo);
+});
